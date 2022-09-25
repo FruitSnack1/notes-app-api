0a281a3fe8adeeec84269d6833af3c4621804707
FruitSnack1 <tomas.ryzner@email.cz>
2022-09-25 14:37:40 +0200

    cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import './src/configs/db.js'
 import router from './src/routes/router.js'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use('/', router)
 
 const port = 3001

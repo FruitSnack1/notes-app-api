2f94bc761f4f55e9ffaec8ae464d444925159ad4
FruitSnack1 <tomas.ryzner@email.cz>
2022-10-29 10:39:25 +0200

    cors fix

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(
-  cors({ credentials: true, origin: 'https://notes-app-amber.vercel.app/' })
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://notes-app-amber.vercel.app/'],
+  })
 )
 app.use('/', router)
 
